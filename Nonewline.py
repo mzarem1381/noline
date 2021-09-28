d0a50ece587648da417b291d4bebadeb30f4f3f6
mzarem1381 <90952218+mzarem1381@users.noreply.github.com>
2021-09-28 10:46:02 +0330

    Add files via upload

---
+++ Nonewline.py
@@ -0,0 +1,16 @@
+# Python 3
+#Install on Linux/macOS: pip3 install pyperclip
+#On Linux, this module makes use of the xclip or xsel commands, which should come with the os. Otherwise run "sudo apt-get install xclip"
+import pyperclip
+def removeline(text):
+    new = ""
+    for i in text:
+        if i != "\n":
+            new = new+i
+    return new
+
+text= pyperclip.paste()
+noline= removeline(text)
+print (noline)
+pyperclip.copy(noline)
+
